c6c150844ce9cf9729c1c20403591e8b2383e1e6
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-03-21 15:31:22 +0530

    commit with completed code of the workout tracking application using api keys and google sheets.

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import requests
+from datetime import datetime
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+GENDER = input("Please enter Your Gender Here:")
+WEIGHT_KG = input("Enter Your Weight Here:")
+HEIGHT_CM = input("Enter Your Height Here:")
+AGE = input("Enter Your Age Here:")
 
+APP_ID = os.environ["NT_APP_ID"]
+API_KEY = os.environ["NT_APP_KEY"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+exercise_endpoint = "https://trackap.mutritionix.com/v2/natural/exercise"
+sheet_endpoint = os.environ["SHEET_ENDPOINT"]
 
+exercise_text = input("Tell Me Which Exercise You Did: ")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+authorization_headers = {
+    "Authorization": f"Basic {os.environ['TOKEN']}"
+}
+
+for exercise in result["exercise"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(
+        sheet_endpoint, 
+        json=sheet_inputs,
+        auth=(
+            "USERNAME",
+            "PASSWORD",
+        )
+    )
+
+    print(sheet_response.text())
+
